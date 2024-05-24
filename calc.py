3446aa058d5af650bc04f26b5de7d5cb76ed6fe5
Alexandr Prizmo Osipov <roboyess2@yandex.ru>
2024-05-24 14:03:22 +0300

    Добавлено приветственное сообщение

--- calc.py
+++ calc.py
@@ -2,6 +2,9 @@ import tkinter as tk
 
 def click(button_text):
     current_expression = str(entry.get())
+    if current_expression == "Ошибка" or current_expression == "на ноль делить нельзя":
+        entry.delete(0, tk.END)
+        current_expression = ""
     updated_expression = current_expression + str(button_text)
     entry.delete(0, tk.END)
     entry.insert(0, updated_expression)
@@ -9,10 +12,20 @@ def click(button_text):
 def clear():
     entry.delete(0, tk.END)
 
+def square():
+    try:
+        expression = entry.get()
+        result = eval(expression) ** 2
+        entry.delete(0, tk.END)
+        entry.insert(0, result)
+    except:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Ошибка")
+
 def evaluate():
     try:
         expression = entry.get()
-        if '/0' in expression:
+        if '/0' in expression or '/ 0' in expression:
             raise ZeroDivisionError
         result = eval(expression)
         entry.delete(0, tk.END)
@@ -20,7 +33,13 @@ def evaluate():
     except ZeroDivisionError:
         entry.delete(0, tk.END)
         entry.insert(0, "на ноль делить нельзя")
-    except:
+    except SyntaxError:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Ошибка")
+    except NameError:
+        entry.delete(0, tk.END)
+        entry.insert(0, "Ошибка")
+    except Exception:
         entry.delete(0, tk.END)
         entry.insert(0, "Ошибка")
 
@@ -28,24 +47,30 @@ window = tk.Tk()
 window.title("Калькулятор")
 window.geometry("470x500")  # Увеличиваем размер окна
 
+# Приветственное сообщение
+tk.Label(window, text="Добро пожаловать в Калькулятор!", font=('Arial', 20)).grid(row=0, column=0, columnspan=4)
+
 entry = tk.Entry(window, width=30, font=('Arial', 20))
-entry.grid(row=0, column=0, columnspan=4)
+entry.grid(row=1, column=0, columnspan=4)
 
 buttons = [
     '7', '8', '9', '/',
     '4', '5', '6', '*',
     '1', '2', '3', '-',
-    '0', '.', '=', '+'
+    '0', '.', '=', '+',
+    '^2'  # Кнопка возведения в квадрат
 ]
 
-row_val = 1
+row_val = 2
 col_val = 0
 
 for button in buttons:
-    action = lambda x = button: click(x)
     if button == "=":
         tk.Button(window, text=button, width=5, height=2, font=('Arial', 20), command=evaluate).grid(row=row_val, column=col_val)
+    elif button == "^2":
+        tk.Button(window, text=button, width=5, height=2, font=('Arial', 20), command=square).grid(row=row_val, column=col_val)
     else:
+        action = lambda x = button: click(x)
         tk.Button(window, text=button, width=5, height=2, font=('Arial', 20), command=action).grid(row=row_val, column=col_val)
     col_val += 1
     if col_val > 3:
